76895dfd121abe3d13f8200924663349890da02a
Marred <nechoya@gmail.com>
2017-04-01 15:52:36 +0200

    Exercises 1.3 and 1.4

---
+++ Part 1/BMI/src/bmi/BMI.java
@@ -0,0 +1,50 @@
+/*
+ * Napisać program, który oblicza wartość współczynnika BMI (ang. body mass
+ * index) wg. wzoru: waga
+ * wzrost2 . Jeżeli wynik jest w przedziale (18,5 - 24,9) to wypisuje
+ * ”waga prawidłowa”, jeżeli poniżej to ”niedowaga”, jeżeli powyżej ”nadwaga”.
+ */
+package bmi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mariusz Redzimski
+ */
+public class BMI {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        float weight = 0;
+        float height = 0;
+        float bmi;
+
+        System.out.println("Please input your weight in kilograms");
+
+        try {
+            weight = scanner.nextFloat();
+        } catch (Exception e) {
+            System.out.println("Incorrect input");
+        }
+
+        System.out.println("Please input your height in centimeters");
+
+        try {
+            height = scanner.nextFloat() / 100;
+        } catch (Exception e) {
+            System.out.println("Incorrect input");
+        }
+
+        bmi = weight / (float) Math.pow(height, 2);
+        if (bmi < 18.5) {
+            System.out.printf("You are underweight, your BMI is %.1f", bmi);
+        } else if (bmi > 24.9) {
+            System.out.printf("You are overweight, your BMI is %.1f", bmi);
+        } else {
+            System.out.printf("Congratulations, your weight is normal, your BMI is %.1f", bmi);
+        }
+    }
+
+}
---
+++ Part 1/Taxation/src/taxation/Taxation.java
@@ -0,0 +1,43 @@
+/*
+ * Napisać program obliczający należny podatek dochodowy od osób fizycznych. Program
+ * ma pobierać od użytkownika dochód i po obliczeniu wypisywać na ekranie
+ * należny podatek. Podatek obliczany jest wg. następujących reguł:
+ * • do 85.528 podatek wynosi 18% podstawy minus 556,02 PLN,
+ * • od 85.528 podatek wynosi 14.839,02 zł + 32% nadwyżki ponad 85.528,00
+ */
+package taxation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mariusz Redzimski
+ */
+public class Taxation {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int income = 0;
+        float tax;
+        
+        System.out.println("Please input your yearly income");
+        
+        try {
+            income = scanner.nextInt();
+        }
+        catch(Exception e) {
+            System.out.println("Incorrect input");
+        }
+        
+        if(income<85528) {
+            tax = (income * 0.18f) - 556.02f;
+        }
+        else {
+            tax = 14839.02f+((income-85528)*0.32f);
+        }
+        
+        System.out.printf("The tax amount for your %d of income is %.2f", income, tax);
+    }
+    
+}
